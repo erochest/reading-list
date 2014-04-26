fd8daf67235aa404eb3964fb66399e9ebdea7d8c
Eric Rochester <erochest@virginia.edu>
2014-04-25 23:24:41 -0400

    Added scaffolding for command-line options.

--- ReadingList.hs
+++ ReadingList.hs
@@ -4,5 +4,20 @@
 module Main where
 
 
+import           Options.Applicative
+
+
 main :: IO ()
-main = undefined
+main = print =<< execParser opts
+
+data ReadingList = RL
+                 deriving (Show)
+
+opts :: ParserInfo ReadingList
+opts = info (helper <*> readingList)
+            (  fullDesc
+            <> progDesc "Generate EPUBs from a reading list."
+            <> header "reading-list -- generate EPUBs")
+
+readingList :: Parser ReadingList
+readingList = pure RL
