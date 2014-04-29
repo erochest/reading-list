d094000ce36ad49381ebe5fe82ffdb2d65c30d4b
Eric Rochester <erochest@virginia.edu>
2014-04-28 21:10:20 -0400

    Read in the URLs from an input text file.

--- ReadingList.hs
+++ ReadingList.hs
@@ -1,14 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 
 module Main where
 
 
+import           Control.Error.Script
+import           Control.Lens
+import qualified Data.Text            as T
+import           Filesystem
+import           Network.Wreq
 import           Options.Applicative
+import qualified Filesystem.Path.CurrentOS as FS
 
-import ReadingList.Options
+import           ReadingList.Options
+import           ReadingList.Types
 
 
 main :: IO ()
-main = print =<< execParser readingListOptions
+main = runScript . readingList =<< execParser readingListOptions
+
+
+readingList :: ReadingList -> Script ()
+readingList RL{..} = do
+    scriptIO $ createTree rlHtmlDir
+    scriptIO (readTextFile rlInputFile) >>=
+        mapM_ (downloadLink rlHtmlDir) . T.lines
+
+downloadLink :: FS.FilePath -> T.Text -> Script FS.FilePath
+downloadLink = undefined
 
