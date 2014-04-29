a7f3414859b94ba11fb4837ed8b60ef4e2682d4b
Eric Rochester <erochest@virginia.edu>
2014-04-28 20:46:09 -0400

    Added some command-line options.

--- ReadingList.hs
+++ ReadingList.hs
@@ -4,14 +4,18 @@
 module Main where
 
 
+import qualified Filesystem.Path.CurrentOS as FS
 import           Options.Applicative
 
 
 main :: IO ()
 main = print =<< execParser opts
 
-data ReadingList = RL
-                 deriving (Show)
+data ReadingList
+        = RL
+        { rlInputFile :: FS.FilePath
+        , rlHtmlDir   :: FS.FilePath
+        } deriving (Show)
 
 opts :: ParserInfo ReadingList
 opts = info (helper <*> readingList)
@@ -19,5 +23,23 @@ opts = info (helper <*> readingList)
             <> progDesc "Generate EPUBs from a reading list."
             <> header "reading-list -- generate EPUBs")
 
+filePathReader :: Mod OptionFields FS.FilePath
+filePathReader = reader (return . FS.decodeString)
+
+filePathOption :: Mod OptionFields FS.FilePath -> Parser FS.FilePath
+filePathOption options = nullOption (filePathReader <> options)
+
 readingList :: Parser ReadingList
-readingList = pure RL
+readingList =   RL
+            <$> filePathOption
+                (  long "input"
+                <> short 'i'
+                <> metavar "INPUT-FILE"
+                <> help "The input file containing URLs to read.")
+            <*> filePathOption
+                (  long "html-dir"
+                <> short 'H'
+                <> metavar "HTML-DIR"
+                <> value "./html"
+                <> help "The working directory to store the HTML files in.")
+
