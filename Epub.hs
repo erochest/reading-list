168d0884cec0cfecc20441b83ccc92b84c7623e6
Eric Rochester <erochest@virginia.edu>
2014-08-29 11:53:00 -0400

    Download the chapters.

--- Epub.hs
+++ Epub.hs
@@ -5,12 +5,14 @@ module Main where
 
 
 import           Control.Lens
+import           Control.Monad
 import           Data.Aeson
 import           Data.Aeson.TH
 import qualified Data.ByteString.Lazy      as B
 import           Data.Char                 (toLower)
 import qualified Data.Text                 as T
 import           Filesystem.Path.CurrentOS hiding (decode)
+import           Network.Wreq
 import           Prelude                   hiding (FilePath)
 
 import           Epub.Options
@@ -18,7 +20,12 @@ import           Epub.Types
 
 main :: IO ()
 main = do
-    o <- execParser opts'
-    print o
-    e <- (fmap decode . B.readFile . encodeString $ _epubSpec o) :: IO (Maybe Epub)
-    print e
+    o     <- execParser opts'
+    mspec <- fmap decode . B.readFile . encodeString $ _epubSpec o
+
+    case mspec of
+        Nothing   -> putStrLn "ERROR: Invalid epub spec file."
+        Just spec -> forM_ (_epubChapters spec) $ \url -> do
+            let url' = T.unpack url
+            putStrLn url'
+            print . (^. responseStatus . statusCode) =<< get url'
